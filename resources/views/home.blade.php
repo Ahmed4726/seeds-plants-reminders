5b5dedc737126fe52fc44205ef0b08ab8822601f
Ahmed4726 <ma7985067@gmail.com>
2024-06-10 20:30:58 +0500

    ok

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container">
+        <h1>Welcome to the Home Page</h1>
+        <!-- Additional content can go here -->
+    </div>
+@endsection
